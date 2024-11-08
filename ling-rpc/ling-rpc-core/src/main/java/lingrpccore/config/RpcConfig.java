b302d7ad0ea3920b75817302d0420d84f7b9c887
ling <1710247412@qq.com>
2024-11-08 08:38:55 +0800

    使用配置实现rpc-core

--- ling-rpc/ling-rpc-core/src/main/java/lingrpccore/config/RpcConfig.java
+++ ling-rpc/ling-rpc-core/src/main/java/lingrpccore/config/RpcConfig.java
@@ -22,10 +22,15 @@ public class RpcConfig {
      * 服务器主机名
      */
     private String serverHost = "localhost";
-    
+
     /**
      * 服务器端口号
      */
     private Integer serverPort = 9800;
+
+    /**
+     * 模拟调用
+     */
+    private boolean mock = false;
 
 }
